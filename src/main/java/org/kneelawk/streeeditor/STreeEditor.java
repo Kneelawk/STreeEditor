63d86d08c99e415432aba3a6f452731a70716ecf
Kneelawk <jedidiah.pommert@gmail.com>
2015-05-30 00:47:30 -0700

    Initial Upload Commit

---
+++ src/main/java/org/kneelawk/streeeditor/STreeEditor.java
@@ -0,0 +1,16 @@
+package org.kneelawk.streeeditor;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class STreeEditor extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+}
